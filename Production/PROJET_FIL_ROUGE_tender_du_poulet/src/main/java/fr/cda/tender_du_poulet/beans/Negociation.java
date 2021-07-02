e3ec374d0b1acea56bf87c99af2bbde5c208ff81
Camille-Binet <cam.binet@gmail.com>
2021-07-02 16:35:57 +0200

    envoi Negociation.java

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/Negociation.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/Negociation.java
@@ -1,16 +1,35 @@
 package fr.cda.tender_du_poulet.beans;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Negociation {
+public class Negociation implements Serializable{
 	
 	@Id
 	@GeneratedValue( strategy=GenerationType.IDENTITY)
 	int idNegociation;
+
+	public Negociation(int idNegociation) {
+		super();
+		this.idNegociation = idNegociation;
+	}
+
+	public int getIdNegociation() {
+		return idNegociation;
+	}
+
+	public void setIdNegociation(int idNegociation) {
+		this.idNegociation = idNegociation;
+	}
+	
+	
+	
+	
 	
 
 }
