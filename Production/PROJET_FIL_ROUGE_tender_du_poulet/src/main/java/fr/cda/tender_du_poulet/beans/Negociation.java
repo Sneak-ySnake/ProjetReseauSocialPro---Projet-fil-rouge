e61c6cfdf617be1325defef87acb0bddc5d2fb75
Camille-Binet <cam.binet@gmail.com>
2021-07-02 11:40:15 +0200

    Ajout Negociation.java

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/Negociation.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/Negociation.java
@@ -2,13 +2,14 @@ package fr.cda.tender_du_poulet.beans;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Negociation {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue( strategy=GenerationType.IDENTITY)
 	int idNegociation;
 	
 
