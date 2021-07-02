cfdfff12d71229094539c78cd301fc4b4c967a52
Théo <theomaes54@gmail.com>
2021-07-02 16:11:02 +0200

    ajout beans et dao - Théo 02/07/2021

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/AccesPublication.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/AccesPublication.java
@@ -6,8 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "acces_publication")
 public class AccesPublication {
 
 	@Id
@@ -21,7 +23,7 @@ public class AccesPublication {
 	public AccesPublication() {
 		
 	}
-	
+
 	public AccesPublication(AccesPublicationId acces_publication_id, boolean accessible) {
 		this.acces_publication_id = acces_publication_id;
 		this.accessible = accessible;
