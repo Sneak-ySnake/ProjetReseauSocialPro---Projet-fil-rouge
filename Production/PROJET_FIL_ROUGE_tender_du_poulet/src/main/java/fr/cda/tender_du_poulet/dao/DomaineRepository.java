c901762257efd6bf55087a8f138e4329e92a6c3b
Camille-Binet <cam.binet@gmail.com>
2021-07-05 14:55:30 +0200

    ModifDomaine Repository

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dao/DomaineRepository.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dao/DomaineRepository.java
@@ -2,6 +2,10 @@ package fr.cda.tender_du_poulet.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DomaineRepository extends JpaRepository<DomaineRepository, Integer>{
+import fr.cda.tender_du_poulet.beans.Domaine;
+
+public interface DomaineRepository extends JpaRepository<Domaine, Integer>{
+
+	 
 
 }
