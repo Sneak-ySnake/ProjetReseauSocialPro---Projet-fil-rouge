e01e82813d9b2f414dce4149604f94d1efdbb5dc
Camille-Binet <cam.binet@gmail.com>
2021-07-06 14:30:13 +0200

    Modifs FavoriID, negocier, negocierId

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/FavoriId.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/FavoriId.java
@@ -5,48 +5,40 @@ import java.io.Serializable;
 import javax.persistence.Embeddable;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
-@Embeddable
+@Embeddable 
 public class FavoriId  implements Serializable {
 	
 	@ManyToOne()
 	@JoinColumn(name = "id_utilisateur")
 	private Utilisateur utilisateur;
 	
-	
 	@JoinColumn(name = "id_utilisateur")
 	private int id_utilisateur_favori;
 
-
+   public FavoriId() {}
+   
 	public FavoriId(Utilisateur utilisateur, int id_utilisateur_favori) {
-		super();
 		this.utilisateur = utilisateur;
 		this.id_utilisateur_favori = id_utilisateur_favori;
 	}
-
-
+	
 	public Utilisateur getUtilisateur() {
 		return utilisateur;
 	}
-
+	
 
 	public void setUtilisateur(Utilisateur utilisateur) {
 		this.utilisateur = utilisateur;
 	}
 
-
 	public int getId_utilisateur_favori() {
 		return id_utilisateur_favori;
 	}
 
-
 	public void setId_utilisateur_favori(int id_utilisateur_favori) {
 		this.id_utilisateur_favori = id_utilisateur_favori;
 	}
-
  
-	
-	
-	
-	
 }
