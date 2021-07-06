4a120b0b6a0d708c5f04a115461dd198a28c4af9
Camille-Binet <cam.binet@gmail.com>
2021-07-06 14:22:48 +0200

    modif domaine
    @Entity table etc...

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/Domaine.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/beans/Domaine.java
@@ -5,8 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+@Entity @Table(name="domaine")
 public class Domaine {
 	
 	@Id 
