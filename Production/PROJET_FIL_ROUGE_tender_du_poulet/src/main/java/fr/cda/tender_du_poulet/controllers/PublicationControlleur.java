2ddd6b0f4246e2ea8ae4feabc10be7dbb3f35aa6
Théo <theomaes54@gmail.com>
2021-07-15 15:27:36 +0200

    ajout publication controlleur - Théo 15/07/2021

---
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/controllers/PublicationControlleur.java
@@ -0,0 +1,24 @@
+package fr.cda.tender_du_poulet.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.cda.tender_du_poulet.dao.PublicationRepository;
+import fr.cda.tender_du_poulet.dto.PublicationDTO;
+import fr.cda.tender_du_poulet.service.PublicationService;
+
+@RestController
+public class PublicationControlleur {
+
+	private PublicationService service = new PublicationService();
+	
+	@Autowired 
+	PublicationRepository publicationRepository;
+	
+	@GetMapping(value = "/ajoutPublication")
+	public void AjoutPublication(PublicationDTO p) {
+		service.ajoutPublication(p);
+	}
+	
+}
