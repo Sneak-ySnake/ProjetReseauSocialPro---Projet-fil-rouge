7eb0f0be59de2ca67c7ac2a2af3978d03c932e3a
Camille-Binet <cam.binet@gmail.com>
2021-07-15 14:49:41 +0200

    DTO Convert Negociation

---
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dto/NegociationDTO.java
@@ -0,0 +1,23 @@
+package fr.cda.tender_du_poulet.dto;
+
+public class NegociationDTO {
+	
+	int id_negociation;
+
+	public NegociationDTO(int id_negociation) {
+		 
+		this.id_negociation = id_negociation;
+	}
+
+	public int getId_negociation() {
+		return id_negociation;
+	}
+
+	public void setId_negociation(int id_negociation) {
+		this.id_negociation = id_negociation;
+	}
+	
+	
+	
+
+}
