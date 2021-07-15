54c59609397a570c0a93575bb301186e499309ba
Théo <theomaes54@gmail.com>
2021-07-15 15:31:04 +0200

    ajout controller - Théo 15/07/2021

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/controllers/PublicationController.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/controllers/PublicationController.java
@@ -1,10 +1,8 @@
 package fr.cda.tender_du_poulet.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.cda.tender_du_poulet.dao.PublicationRepository;
 import fr.cda.tender_du_poulet.dto.PublicationDTO;
 import fr.cda.tender_du_poulet.service.PublicationService;
 
@@ -12,9 +10,6 @@ import fr.cda.tender_du_poulet.service.PublicationService;
 public class PublicationController {
 
 	private PublicationService service = new PublicationService();
-	
-	@Autowired 
-	PublicationRepository publicationRepository;
 	
 	@GetMapping(value = "/ajoutPublication")
 	public void AjoutPublication(PublicationDTO p) {
