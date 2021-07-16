277c936901e1bcf0cffcf59b37483527ae9a2be8
LoicMathieu62 <loicmathieu62@hotmail.fr>
2021-07-16 12:31:15 +0200

    MAJ DTO de Utilisateur_EtatCompte / Utilisateur_EtatCompteId

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dto/Utilisateur_EtatCompteDTO.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dto/Utilisateur_EtatCompteDTO.java
@@ -4,24 +4,22 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
-import fr.cda.tender_du_poulet.beans.Utilisateur_EtatCompteId;
-
 public class Utilisateur_EtatCompteDTO {
 	@EmbeddedId @GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Utilisateur_EtatCompteId utilisateur_EtatCompteId;
+	private Utilisateur_EtatCompteIdDTO utilisateur_EtatCompteIdDTO;
 	
 	
 /////////////////////////////// CONSTRUCTEUR ///////////////////////////////////////////////////////////////////////////
-	public Utilisateur_EtatCompteDTO(Utilisateur_EtatCompteId utilisateur_EtatCompteId) {
-		this.utilisateur_EtatCompteId = utilisateur_EtatCompteId;
+	public Utilisateur_EtatCompteDTO(Utilisateur_EtatCompteIdDTO utilisateur_EtatCompteIdDTO) {
+		this.utilisateur_EtatCompteIdDTO = utilisateur_EtatCompteIdDTO;
 	}
 
 
 /////////////////////////////// GETTER / SETTER ///////////////////////////////////////////////////////////////////////////
-	public Utilisateur_EtatCompteId getUtilisateur_EtatCompteId() {
-		return utilisateur_EtatCompteId;
+	public Utilisateur_EtatCompteIdDTO getUtilisateur_EtatCompteIdDTO() {
+		return utilisateur_EtatCompteIdDTO;
 	}
-	public void setUtilisateur_EtatCompteId(Utilisateur_EtatCompteId utilisateur_EtatCompteId) {
-		this.utilisateur_EtatCompteId = utilisateur_EtatCompteId;
+	public void setUtilisateur_EtatCompteIdDTO(Utilisateur_EtatCompteIdDTO utilisateur_EtatCompteIdDTO) {
+		this.utilisateur_EtatCompteIdDTO = utilisateur_EtatCompteIdDTO;
 	}
 }
--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dto/Utilisateur_EtatCompteIdDTO.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dto/Utilisateur_EtatCompteIdDTO.java
@@ -2,36 +2,33 @@ package fr.cda.tender_du_poulet.dto;
 
 import java.util.Date;
 
-import fr.cda.tender_du_poulet.beans.EtatCompte;
-import fr.cda.tender_du_poulet.beans.Utilisateur;
-
 public class Utilisateur_EtatCompteIdDTO {
-	private Utilisateur utilisateur;
-	private EtatCompte etat_compte;
+	private UtilisateurDTO utilisateurDTO;
+	private EtatCompteDTO etat_compteDTO;
 	private Date date_debut;
 
 	
 /////////////////////////////// CONSTRUCTEUR ///////////////////////////////////////////////////////////////////////////
-	public Utilisateur_EtatCompteIdDTO(Utilisateur utilisateur, EtatCompte etat_compte, Date date_debut) {
-		this.utilisateur = utilisateur;
-		this.etat_compte = etat_compte;
+	public Utilisateur_EtatCompteIdDTO(UtilisateurDTO utilisateurDTO, EtatCompteDTO etat_compteDTO, Date date_debut) {
+		this.utilisateurDTO = utilisateurDTO;
+		this.etat_compteDTO = etat_compteDTO;
 		this.date_debut = date_debut;
 	}
 
 
 /////////////////////////////// GETTER / SETTER ///////////////////////////////////////////////////////////////////////////
-	public Utilisateur getUtilisateur() {
-		return utilisateur;
+	public UtilisateurDTO getUtilisateurDTO() {
+		return utilisateurDTO;
 	}
-	public void setUtilisateur(Utilisateur utilisateur) {
-		this.utilisateur = utilisateur;
+	public void setUtilisateurDTO(UtilisateurDTO utilisateurDTO) {
+		this.utilisateurDTO = utilisateurDTO;
 	}
 	
-	public EtatCompte getEtat_compte() {
-		return etat_compte;
+	public EtatCompteDTO getEtat_compteDTO() {
+		return etat_compteDTO;
 	}
-	public void setEtat_compte(EtatCompte etat_compte) {
-		this.etat_compte = etat_compte;
+	public void setEtat_compteDTO(EtatCompteDTO etat_compteDTO) {
+		this.etat_compteDTO = etat_compteDTO;
 	}
 	
 	public Date getDate_debut() {
