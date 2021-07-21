6dca4250187c33eb294b32599869b984eb6ab5a4
Théo <theomaes54@gmail.com>
2021-07-21 09:38:40 +0200

    maj controlleur Théo 21/07/2021

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/controllers/TypePublicationController.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/controllers/TypePublicationController.java
@@ -3,19 +3,13 @@ package fr.cda.tender_du_poulet.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.cda.tender_du_poulet.beans.TypePublication;
-import fr.cda.tender_du_poulet.dao.TypePublicationRepository;
 import fr.cda.tender_du_poulet.dto.TypePublicationDTO;
-import fr.cda.tender_du_poulet.iService.TypePublicationInterfaceService;
 import fr.cda.tender_du_poulet.service.TypePublicationService;
-import fr.cda.tender_du_poulet.util.TypePublicationConverter;
 
 @RestController
 public class TypePublicationController {
