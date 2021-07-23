1ccf3ab50b2380320e08209650364bd321ecbbf5
LoicMathieu62 <loicmathieu62@hotmail.fr>
2021-07-23 12:08:17 +0200

    Optimisation de Utilisateur_EtatCompteController - 23/07/2021 12:07
    suppression de la methode appelant le repository (findall2)

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/controllers/Utilisateur_EtatCompteController.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/controllers/Utilisateur_EtatCompteController.java
@@ -1,6 +1,5 @@
 package fr.cda.tender_du_poulet.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,11 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.cda.tender_du_poulet.beans.Utilisateur_EtatCompte;
 import fr.cda.tender_du_poulet.beans.Utilisateur_EtatCompteId;
-import fr.cda.tender_du_poulet.dao.Utilisateur_EtatCompteRepository;
 import fr.cda.tender_du_poulet.dto.Utilisateur_EtatCompteDTO;
-import fr.cda.tender_du_poulet.dto.Utilisateur_EtatCompteIdDTO;
 import fr.cda.tender_du_poulet.service.Utilisateur_EtatCompteService;
 
 @RestController
@@ -21,8 +17,6 @@ public class Utilisateur_EtatCompteController {
 	
 	@Autowired
 	private Utilisateur_EtatCompteService utilisateur_EtatCompteService;
-	@Autowired
-	private Utilisateur_EtatCompteRepository utilisateur_EtatCompteRepository;
 	
 	@RequestMapping(value = "/addUtilisateur_EtatCompte", method = RequestMethod.POST)
 	public void ajoutUtilisateur_EtatCompte(@RequestBody Utilisateur_EtatCompteDTO d) {
@@ -38,13 +32,6 @@ public class Utilisateur_EtatCompteController {
 	public List<Utilisateur_EtatCompteDTO> recupAllUtilisateur_EtatCompte() {
 		return utilisateur_EtatCompteService.recupAllUtilisateur_EtatCompte();
 	}
-
-	@RequestMapping(value = "/findAll2Utilisateur_EtatCompte", method = RequestMethod.POST)
-	public List<Utilisateur_EtatCompte> recupAllUtilisateur_EtatCompte2() {
-		List<Utilisateur_EtatCompte> listUtilisateur_EtatCompteDTO = (List<Utilisateur_EtatCompte>) utilisateur_EtatCompteRepository.findAll();
-		return listUtilisateur_EtatCompteDTO;
-	}
-
 
 	@RequestMapping(value = "/updateUtilisateur_EtatCompte", method = RequestMethod.POST)
 	public void modifUtilisateur_EtatCompte(@RequestBody Utilisateur_EtatCompteDTO d) {
