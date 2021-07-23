1984b419a82c5aea6e5f383050ff80eae0057c22
TheoMaes <82203310+TheoMaes@users.noreply.github.com>
2021-07-23 16:11:15 +0200

    Mettez des DTOOOOO!

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dto/AccesPublicationDTO.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dto/AccesPublicationDTO.java
@@ -4,7 +4,7 @@ import fr.cda.tender_du_poulet.beans.AccesPublicationId;
 
 public class AccesPublicationDTO {
 	
-	private AccesPublicationId acces_publication_id;
+	private AccesPublicationIdDTO acces_publication_id;
 	
 	private boolean accessible;
 		
@@ -18,11 +18,11 @@ public class AccesPublicationDTO {
 		return accessible;
 	}
 
-	public AccesPublicationId getAcces_publication_id() {
+	public AccesPublicationIdDTO getAcces_publication_id() {
 		return acces_publication_id;
 	}
 
-	public void setAcces_publication_id(AccesPublicationId acces_publication_id) {
+	public void setAcces_publication_id(AccesPublicationIdDTO acces_publication_id) {
 		this.acces_publication_id = acces_publication_id;
 	}
 
