5eadb964911c292d7529411ebcbc7b94e56dece0
Théo <theomaes54@gmail.com>
2021-07-26 09:42:58 +0200

    accesPublication

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dto/AccesPublicationDTO.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/dto/AccesPublicationDTO.java
@@ -1,7 +1,5 @@
 package fr.cda.tender_du_poulet.dto;
 
-import fr.cda.tender_du_poulet.beans.AccesPublicationId;
-
 public class AccesPublicationDTO {
 	
 	private AccesPublicationIdDTO acces_publication_id;
