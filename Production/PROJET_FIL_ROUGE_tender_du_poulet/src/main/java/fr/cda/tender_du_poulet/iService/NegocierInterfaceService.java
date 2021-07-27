1cdc55d562770bc4d56097645d247f93ffbf6d8b
Camille-Binet <cam.binet@gmail.com>
2021-07-27 15:51:19 +0200

    Update Service Negocier

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/iService/NegocierInterfaceService.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/iService/NegocierInterfaceService.java
@@ -2,11 +2,16 @@ package fr.cda.tender_du_poulet.iService;
 
 import java.util.List;
 
+import fr.cda.tender_du_poulet.beans.NegocierId;
 import fr.cda.tender_du_poulet.dto.NegocierDTO;
  
  public interface NegocierInterfaceService {
  
 	public void ajoutNegocier(NegocierDTO n);  
+	
+    public NegocierDTO recupNegocier(NegocierId id);
+	
+	public List<NegocierDTO> recupAllFavoriNegocier();  
 	
   	
 }
