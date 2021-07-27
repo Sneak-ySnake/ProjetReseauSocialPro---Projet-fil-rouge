8387fab31f7964a226d39e035cfedc218fb6dde5
Théo <theomaes54@gmail.com>
2021-07-27 16:22:10 +0200

    METTEZ LES REQUEST BODY!!!!

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/controllers/PaysController.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/controllers/PaysController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.cda.tender_du_poulet.dto.PaysDTO;
@@ -17,12 +18,12 @@ public class PaysController {
 	private PaysService service;
 	
 	@PostMapping(value = "/ajoutPays")
-	public void ajoutPays(PaysDTO p) {
+	public void ajoutPays(@RequestBody PaysDTO p) {
 		service.ajoutPays(p);
 	}
 	
 	@PostMapping(value = "/recupPays")
-	public PaysDTO recupPays(String id) {
+	public PaysDTO recupPays(@RequestBody String id) {
 		return service.recupPays(Integer.parseInt(id));
 	}
 	
@@ -32,7 +33,7 @@ public class PaysController {
 	}
 	
 	@PostMapping(value = "/supprimerPays")
-	public void supprimerPays(String id) {
+	public void supprimerPays(@RequestBody String id) {
 		service.supprimerPays(Integer.parseInt(id));
 	}
 	
