847bc267f9dd2c1f5561650624ea5f832ffe5d97
LoicMathieu62 <loicmathieu62@hotmail.fr>
2021-07-28 11:22:27 +0200

    Update Utilisateur_EtatCompte DTO - 28/07/2021 11:22
    test CRUD OK

--- Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/iService/Utilisateur_EtatCompteInterfaceService.java
+++ Production/PROJET_FIL_ROUGE_tender_du_poulet/src/main/java/fr/cda/tender_du_poulet/iService/Utilisateur_EtatCompteInterfaceService.java
@@ -2,18 +2,18 @@ package fr.cda.tender_du_poulet.iService;
 
 import java.util.List;
 
-import fr.cda.tender_du_poulet.beans.Utilisateur_EtatCompteId;
 import fr.cda.tender_du_poulet.dto.Utilisateur_EtatCompteDTO;
+import fr.cda.tender_du_poulet.dto.Utilisateur_EtatCompteIdDTO;
 
 public interface Utilisateur_EtatCompteInterfaceService {
 
 	public void ajoutUtilisateur_EtatCompte(Utilisateur_EtatCompteDTO uec);
 	
-	public Utilisateur_EtatCompteDTO recupUtilisateur_EtatCompte(Utilisateur_EtatCompteId id);
+	public Utilisateur_EtatCompteDTO recupUtilisateur_EtatCompte(Utilisateur_EtatCompteIdDTO id);
 	
 	public List<Utilisateur_EtatCompteDTO> recupAllUtilisateur_EtatCompte();
 	
 	public void modifUtilisateur_EtatCompte(Utilisateur_EtatCompteDTO d);
 	
-	public void supprimerUtilisateur_EtatCompte(Utilisateur_EtatCompteId id);
+	public void supprimerUtilisateur_EtatCompte(Utilisateur_EtatCompteIdDTO id);
 }
